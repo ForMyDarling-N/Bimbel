f992fe0bc6b9d091c7802068fcd835596963f1f1
For My Darling <formydarling1305@gmail.com>
2026-06-20 15:47:54 +0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,6 +3,35 @@ const nextConfig = {
   images: {
     domains: ['firebasestorage.googleapis.com'],
   },
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback = {
+        fs: false,
+        net: false,
+        tls: false,
+        'node:stream': false,
+        'node:buffer': false,
+        'node:util': false,
+        'node:url': false,
+        'node:events': false,
+        'node:path': false,
+        'node:os': false,
+        'node:http': false,
+        'node:https': false,
+        'node:zlib': false,
+        'node:crypto': false,
+        'node:child_process': false,
+        'node:worker_threads': false,
+        'node:readline': false,
+        'node:vm': false,
+        'node:module': false,
+      }
+    }
+    return config
+  },
+  experimental: {
+    esmExternals: false,
+  },
 }
 
 module.exports = nextConfig
