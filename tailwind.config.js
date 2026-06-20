921833f2b7be753c77d3749818a3dfbb9bdec9ce
For My Darling <formydarling1305@gmail.com>
2026-06-20 11:35:09 +0700

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        navy: '#1e3a8a',
+        yellow: '#facc15',
+      },
+    },
+  },
+  plugins: [],
+}
